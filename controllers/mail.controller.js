05258d68c1b0530553f4c0715c55fde0e51a465e
russellmars <russellmars@sina.com>
2018-07-06 10:47:37 +0800

    申请邮箱

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -1,1 +1,43 @@
-// TODO:
+const puppeteer = require('puppeteer');
+const uuidv1 = require('uuid/v1');
+
+const mailServer = 'https://outlook.live.com/owa/';
+const password = 'mars123456';
+
+const CREATE_ACCOUNT_BUTTON_SELECTOR =
+  'body > section > div > div > div.landing-section.headerHero > a.linkButtonSigninHeader.linkButtonSigninHeader--premium';
+const ACCOUNT_INPUT_SELECTOR = '#MemberName';
+const ACCOUNT_INPUT_NEXT_SELECTOR = '#iSignupAction';
+const PASSWORD_INPUT_SELECTOR = '#PasswordInput';
+const PASSWORD_INPUT_NEXT_SELECTOR = '#iSignupAction';
+
+/**
+ * 创建邮箱账户
+ */
+exports.create = async function create(ctx, next) {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1036,
+    height: 700
+  });
+  await page.goto(mailServer);
+
+  const navigationPromise0 = page.waitForNavigation();
+  await page.click(CREATE_ACCOUNT_BUTTON_SELECTOR);
+  await navigationPromise0;
+
+  const account_id = `mars${uuidv1()
+    .replace(/-/g, '')
+    .slice(10, 20)}`;
+  await page.type(ACCOUNT_INPUT_SELECTOR, account_id);
+  await page.click(ACCOUNT_INPUT_NEXT_SELECTOR);
+  await page.waitFor(2000);
+
+  await page.type(PASSWORD_INPUT_SELECTOR, password);
+  await page.click(PASSWORD_INPUT_NEXT_SELECTOR);
+  await page.waitForNavigation();
+};
--- core/router.js
+++ core/router.js
@@ -1,11 +1,15 @@
 const Router = require('koa-router');
-const serverConfig = require('../config/server.json')
+const serverConfig = require('../config/server.json');
 const router = new Router({ prefix: serverConfig.apiRoot });
+
+const mailController = require('../controllers/mail.controller')
 
 router.get('/', (ctx, next) => {
   ctx.body = {
     message: 'hello world'
   };
 });
+
+router.post('/mail', mailController.create)
 
 module.exports = router;
